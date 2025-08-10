b2f9308282538aba61ef5763d569b5d6fa112d27
Kalpana-U <kalpanathangarani578@gmail.com>
2025-08-10 19:26:07 +0530

    updated src file

--- stats.c
+++ stats.c
@@ -1,6 +1,7 @@
 #include "stats.h"
 #include <stddef.h>
 #include <float.h>
+#include <math.h>
 
 struct Stats compute_statistics(const float* numbers, int count) {
     struct Stats stats;
@@ -20,5 +21,10 @@ struct Stats compute_statistics(const float* numbers, int count) {
     }
 
     stats.average = sum/count;
+
+    if (stats.average == NAN) {
+        return stats;
+    }
+    
     return stats;
 }
